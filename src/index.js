934d3928f27494b896bafc35804ec476147b35eb
tracht <marija.trachtenberg@gmail.com>
2020-08-10 16:36:30 +0100

    Removes comment.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import CardSection from './CardSection'
-// import Cards from './Cards.js'
 
 ReactDOM.render(
   <React.StrictMode>
