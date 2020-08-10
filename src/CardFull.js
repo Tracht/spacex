bdd684cbbcc496d1489f8b8a187f89b47647a9f6
tracht <marija.trachtenberg@gmail.com>
2020-08-10 17:39:05 +0100

    Adjusts image size using Figure reactstrap.

--- src/CardFull.js
+++ src/CardFull.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Image from 'react-bootstrap/Image';
+// import Image from 'react-bootstrap/Image';
+import Figure from 'react-bootstrap/Figure';
 
 import "./app.css";
 
@@ -41,7 +42,14 @@ class CardFull extends React.Component {
           <p> Cost_per_launch: {rocket.cost_per_launch} </p>
           <p> First flight: {rocket.first_flight} </p>
           <p> Country: {rocket.country}</p>
-          <Image src={rocket.flickr_images} rounded />
+          <Figure>
+            <Figure.Image
+              width={171*2}
+              height={180*2}
+              alt="Rocket"
+              src={rocket.flickr_images}
+            />
+          </Figure> 
           <br></br>
         </section>
       </div>
