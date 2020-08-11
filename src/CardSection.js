1b8b36dbf680d35f26c16c9601853c0529d87552
tracht <marija.trachtenberg@gmail.com>
2020-08-11 11:26:59 +0100

    CardSummary button is smaller and grey.

--- src/CardSection.js
+++ src/CardSection.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import CardSummary from './CardSummary.js';
 import CardFull from './CardFull.js';
+
 class CardSection extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       data: [],
-      showFullCard: true,
     };
   };
 
@@ -15,14 +15,15 @@ class CardSection extends React.Component {
     return(
       <div>
         <CardSummary />
-        {/* { this.showFulLCard ? <CardFull /> : null } */}
-        <br></br>
+        {/* { !this.showFullCard ? <CardFull /> : "Nothing to see here"}
+        <br></br> */}
         <CardFull />
       </div>
     );
-   }
-    
-
+  }
 }
 
 export default CardSection;
+
+
+
